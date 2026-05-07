e231cfe30404a31ed0530e453b45f827d59a124f
avinaashksingh <avinash.k.singh1797@gmail.com>
2026-05-07 12:21:37 +0530

    Initial commit

---
+++ src/main/java/com/scetzhbook/exchangePipeline/ExchangePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.scetzhbook.exchangePipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scetzhbook/exchangePipeline/ExchangePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scetzhbook.exchangePipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
